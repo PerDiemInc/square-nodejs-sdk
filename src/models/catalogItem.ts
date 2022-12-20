3314143f9e723aa6dca34b1d42190a22aa48c172
Muhammad Javed <theahmadzai@hotmail.com>
2022-12-20 17:36:16 +0500

    Fix ecomImageUris array type

--- src/models/catalogItem.ts
+++ src/models/catalogItem.ts
@@ -156,5 +156,5 @@ export const catalogItemSchema: Schema<CatalogItem> = object({
   descriptionPlaintext: ['description_plaintext', optional(string())],
   ecomAvailable: ['ecom_available', optional(nullable(boolean()))],
   ecomVisibility: ['ecom_visibility', optional(nullable(string()))],
-  ecomImageUris: ['ecom_image_uris', optional(nullable(string()))],
+  ecomImageUris: ['ecom_image_uris', optional(nullable(array(string())))],
 });
